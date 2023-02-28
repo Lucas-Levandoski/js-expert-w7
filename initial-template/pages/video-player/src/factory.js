486931372901e6e8b1220a237abedd110d11de38
Lucas Levandoski <lucas-levandoski@hotmail.com>
2023-02-27 22:47:00 -0300

    camera library working just fine

--- initial-template/pages/video-player/src/factory.js
+++ initial-template/pages/video-player/src/factory.js
@@ -1,6 +1,8 @@
-import { Controller } from "./controller.js";
+import { Controller } from './controller.js'
+import { Camera } from '../../../lib/shared/camera.js'
 
 const [rootPath] = window.location.href.split('/pages/')
+const camera = await Camera.init()
 
 export const factory = {
   async initialize() {
